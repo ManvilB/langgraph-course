8d2666fd240cb2221ff43f8334914fddec76c619
ManvilB <manvilb@gmail.com>
2026-07-10 14:25:21 -0400

    adaptive-rag

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from graph.graph import app
 
 if __name__ == "__main__":
     print("Hi")
-    print(app.invoke(input={"question": "what is agent memory?"}))
+    print(app.invoke(input={"question": "what is pizzaa?"}))
