1ab98d3791a9c9b09a67cc643323db0111853c78
ManvilB <manvilb@gmail.com>
2026-07-10 14:07:43 -0400

    self-rag

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -4,6 +4,7 @@ load_dotenv()
 
 
 from graph.chains.retrieval_grader import GradeDocuments, retrieval_grader
+from graph.chains.hallucination_grader import hallucination_grader, GradeHallucinations
 from ingestion import retriever
 from graph.chains.generation import generation_chain
 
@@ -37,4 +38,25 @@ def test_generation_chain() -> None:
     generation = generation_chain.invoke({"context": docs, "question": question})
     pprint(generation)
 
-    
+
+def test_hallucination_grader_answer_yes() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    generation = generation_chain.invoke({"context": docs, "question": question})
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {"documents": docs, "generation": generation}
+    )
+
+    assert res.binary_score
+
+
+def test_hallucination_grader_answer_no() -> None:
+    question = "agent memory"
+    docs = retriever.invoke(question)
+
+    res: GradeHallucinations = hallucination_grader.invoke(
+        {"documents": docs, "generation": "In order to make pizza we need to first start with the dough",}
+    )
+
+    assert not res.binary_score
