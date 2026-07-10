b895e2f2c17915a7e3787417557ffedcae01ac86
ManvilB <manvilb@gmail.com>
2026-07-10 13:26:22 -0400

    rag-agent

---
+++ graph/chains/generation.py
@@ -0,0 +1,8 @@
+from langsmith import Client
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(temperature=0)
+prompt = Client().pull_prompt("rlm/rag-prompt", dangerously_pull_public_prompt=True)
+
+generation_chain = prompt | llm | StrOutputParser()
